9c9bd27af577bd531517217eda28b1467894c1f8
Pavel Grivachev <pavel@fivetran.com>
2021-04-11 22:10:44 +0300

    add the ability to start grpc server

---
+++ zetasql/local_service/remote_server.cc
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <memory>
+#include <string>
+
+#include <grpcpp/grpcpp.h>
+
+#include "zetasql/local_service/local_service_grpc.h"
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+
+namespace zetasql {
+namespace local_service {
+namespace {
+
+extern "C" void RunServer(std::string &address, int port) {
+  std::string server_address_with_port = address + ":" + std::to_string(port);
+  std::cout << "Running server on " << server_address_with_port << "..." << std::endl;
+
+  ZetaSqlLocalServiceGrpcImpl* service =
+        new ZetaSqlLocalServiceGrpcImpl();
+
+  ServerBuilder builder;
+  // Listen on the given address without any authentication mechanism.
+  builder.AddListeningPort(server_address_with_port, grpc::InsecureServerCredentials());
+  // Register "service" as the instance through which we'll communicate with
+  // clients. In this case it corresponds to an *synchronous* service.
+  builder.RegisterService(service);
+  // Finally assemble the server.
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address_with_port << std::endl;
+
+  // Wait for the server to shutdown. Note that some other thread must be
+  // responsible for shutting down the server for this call to ever return.
+  server->Wait();
+}
+}
+}
+}
+
+int main(int argc, char* argv[]) {
+  std::string address = argv[1];
+  std::string port = argv[2];
+  zetasql::local_service::RunServer(address, std::stoi(port));
+  return 0;
+}
